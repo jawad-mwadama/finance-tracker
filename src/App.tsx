3957500cab9f92f029887b128746eab99ba487fe
jawad-mwadama <allymwadama@gmail.com>
2025-11-12 00:00:08 +0300

    wireFrame

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Dashboard from "./components/DashBoard/Dashboard";
+import SideBar from "./components/SideBar/SideBar";
+
 function App() {
-  return <main className="font-family">Hello</main>;
+  return (
+    <main className="grid grid-cols-[220px,1fr] gap-4 bg-stone-100 p-4 font-Inter text-stone-950">
+      <SideBar />
+      <Dashboard />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/DashBoard/Dashboard.tsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <div className="rounded-lg bg-white pb-4 shadow">Dashboard</div>;
+}
+
+export default Dashboard;
