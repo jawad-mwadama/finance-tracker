2059913d0aecab32fb0b6137023edfdaad3793c0
jawad-mwadama <allymwadama@gmail.com>
2025-11-15 23:11:29 +0300

    tried to use legacy redux

---
+++ src/components/Store.js
@@ -0,0 +1,43 @@
+import { createStore } from "@reduxjs/toolkit";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action };
+    case "account/withdraw":
+      return {
+        ...state,
+        balance: state.balance - action,
+      };
+    case "account/requestLoan":
+      if (state > 0) return state;
+      //   later
+      return {
+        ...state,
+        loan: action.payload,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+
+console.log(store);
